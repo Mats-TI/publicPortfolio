7220ad84591edda9fd8bef2d28500a1ac8e932c9
unknown <thapelomatsaung101@gmail.com>
2021-02-04 22:34:05 +0200

    A wroking project directory
    Added working links to projects section. Some css editing.

--- src/Components/projects.js
+++ src/Components/projects.js
@@ -6,33 +6,55 @@ import Gallery_img from "./Media/gallery_logo.png"
 import Chess_img from './Media/Chess pic.jpg'
 import {Carousel} from "3d-react-carousal"
 
-const slides=[<img src={DCproject} />,
-<img src={Chess_img}></img>,
-<img src={Gallery_img}/>
+const slides=[
+<a href="https://thapza101.github.io/gallery" ><img src={Gallery_img}/></a>,
+<a href="https://thapza101.github.io/chessSite" style={{height:"200px"}}><img src={Chess_img} style={{height:"80%"},{width:"50%"}} /></a>,
+<a href="https://thapza101.github.io/DCGame" style={{height:"200px"}}><img src={DCproject} style={{height:"80%"},{width:"50%"}}/></a>,
 ];
+var details=[
+    {Naam:"Queen's Gambit Chess Club",Type:"Website",linksTo:"https://thapza101.github.io/chessSite",nameOfLink:"Go to Queen's Gambit's site"},
+    {Naam:"Gallery", Type:"Web-application", linksTo:"https://thapza101.github.io/gallery",nameOfLink:"Go to the Gallery site"}
+
+]
 
 class Projects extends Component{
+    constructor(){
+        super();
+        this.state={
+            name:"DC Quiz Game",
+            type:"Web-application",
+            link:"https://thapza101.github.io/DCGame",
+            linkName:"Go to DC Quiz Game's Page"
+        }
+    }
+    changeDetails=()=>{
+        var i=0;
+        this.setState({
+            name:details[i].Naam,
+            type:details[i].Type,
+            link:details[i].linksTo,
+            linkName:details[i].nameOfLink
+        },()=>{
+            i=i+1;
+        })
+    }
 
     render(){
         return(
             <div className='projectsDiv' id="ProID">
             <h1><span className="h1">My Projects</span></h1>
             <section className='projectSection'>
-                {/*<h2 className="projectSubHeading">Browse through some of the web-applications and websites created.</h2>*/}
-                <Carousel slides={slides}>
+                <h2 className="projectSubHeading">Browse through some of the web-applications and websites created.<br /> Click on an icon to access an individual Project.</h2>
+                <Carousel slides={slides} >
                 
                 </Carousel>
-                <div className="projectDetails">
+                {/*<div className="projectDetails">
                     <h2>Project Details</h2>
-                    <p>Project name: DC Quiz Game.</p>
-                    <p>Project type: Web-application.</p>
-                    <a href="https://thapza101.github.io/DCGame">Go to DC Quiz Game's Page</a>
-                </div>
-                {/*<div><a href='https://thapza101.github.io/chessSite/'><img className='site2' src={Chess_img} alt='chess website' title="A Chess club Website" /></a></div>
-                <div><a href='https://thapza101.github.io/DCGame/'><img className='site1' src={DCproject} alt='DCgame' title="A DC Quiz Game"/></a></div>
-                
-                <div><a href="https://thapza101.github.io/gallery"><img className="site3" src={Gallery_img} alt="Prime Gallery Builder" title="A Galley builder"/></a></div>
-        */}
+                    <p>Project name:{` ${this.state.name}`}.</p>
+                    <p>Project type: {` ${this.state.type}`}.</p>
+                    <a href={this.state.link}>{this.state.linkName}</a>
+        </div>
+        <button onClick={this.changeDetails}>Change Details</button>*/}
             </section>
             </div>
         )
