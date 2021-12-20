de2776dd9c1684242b8a7984fc857a162b8fbbf0
Mats-TI <thapelomatsaung101@gmail.com>
2021-12-20 17:47:27 +0200

    redesign of the landing page for tablets

--- src/Components/mainNav.js
+++ src/Components/mainNav.js
@@ -5,6 +5,8 @@ import {BrowserRouter as Router,Switch,Route,Link} from 'react-router-dom'
 import Projects from './projects'
 import Wrapper from './wrapper'
 import mainLogo from "./Media/PortforlioLogo.jpg"
+import openMenu from "./Media/Icons/menuIcon.png"
+import closeMenu from "./Media/Icons/closeMenuIcon.png"
 
 
 class Nav extends React.Component{
@@ -18,14 +20,28 @@ class Nav extends React.Component{
     render(){
         return(
             <div id='mainNav'>
-            <a className="mainLogo" href="#Home"><img src={mainLogo} alt="Logo"></img></a>
-                  <ul>
+                <nav className='topNavMobile'>
+                    <img className="hamburgerIcon" src={openMenu} alt="Menu"></img>
+                    <a className="mainLogoMobile" href="#Home"><img src={mainLogo} alt="Logo"></img></a>
+                </nav>
+                <section className="sideNav">
+                
+                    <ul className='sideNavList'>
+                    <li className="homeLink"><a href="#Home"><i className="fa fa-home"/> Home</a></li>
+                    {/*<li className="aboutLink"><a href="#"><i className="fa fa-address-card"/> About</a></li>*/}
+                    <li className="skillsLink"><a href="#"><i className="fa fa-code"/> My Skills </a></li>
+                    <li className="projectLink"><a href="#ProID"><i className="fa fa-briefcase"/> My Projects</a></li>
+                    <li className="contactLink"><a href="#ContactID"><i className="fa fa-phone" /> Get in touch</a></li>
+                    </ul>
+                </section>
+                <a className="mainLogo" href="#Home"><img src={mainLogo} alt="Logo"></img></a>
+                <ul className="mainNavList">
                       <li className="homeLink"><a href="#Home"><i className="fa fa-home"/> Home</a></li>
                       {/*<li className="aboutLink"><a href="#"><i className="fa fa-address-card"/> About</a></li>*/}
                       <li className="skillsLink"><a href="#"><i className="fa fa-code"/> My Skills &#x25BC; </a></li>
                       <li className="projectLink"><a href="#ProID"><i className="fa fa-briefcase"/> My Projects</a></li>
                       <li className="contactLink"><a href="#ContactID"><i className="fa fa-phone" /> Get in touch</a></li>
-                  </ul>    
+                </ul>    
             </div>
         )
     }
