5cad2a0e1a2bdfb9bbd5d89697b479926c451289
Mats-TI <thapelomatsaung101@gmail.com>
2021-12-20 23:28:51 +0200

    Modified Landing Page for desktop and tablet
    -Added a typing effect
    - Changed font
    -Added Visual aid to my occupation

--- src/Components/wrapper.js
+++ src/Components/wrapper.js
@@ -9,6 +9,8 @@ import ReactDOM from 'react-dom'
 import Contact from './Contact'
 import BackToTop from 'react-back-to-top-button'
 import topIcon from "./Media/iconfinder_arrow-up_511953.png"
+import AI from "./Media/brain_electronics_icon.png"
+import DEV from "./Media/2290862_code_dashboard_development_html_text_icon.png"
 
 
 class Wrapper extends React.Component{
@@ -21,9 +23,11 @@ class Wrapper extends React.Component{
         <Router>
         <div>
             <div className="Wrapper" id='Home'>
-                <h1>The name's Thapelo, pleased to make your acquaintance</h1>
+                <h1>The name's Thapelo, pleased to make your acquaintance.</h1>
                 <div className="info">
+                <img src={DEV} alt="DevIcon"></img>
                 <p className="p1">Full Stack Web Developer</p>
+                <img src={AI} alt="AI"></img>
                 <p className="p1">Machine Learning Engineer</p>
 
             </div>
