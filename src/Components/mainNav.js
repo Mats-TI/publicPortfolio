4ab81f9e227374464a24a46afd4832379ef92c8f
Mats-TI <thapelomatsaung101@gmail.com>
2021-12-24 21:51:40 +0200

    Update mainNav.js

--- src/Components/mainNav.js
+++ src/Components/mainNav.js
@@ -61,7 +61,7 @@ class Nav extends React.Component{
                 <ul className="mainNavList">
                       <li className="homeLink"><a href="#Home"><i className="fa fa-home"/> Home</a></li>
                       {/*<li className="aboutLink"><a href="#"><i className="fa fa-address-card"/> About</a></li>*/}
-                      <li className="skillsLink"><a href="#"><i className="fa fa-code"/> My Skills &#x25BC; </a></li>
+                      <li className="skillsLink"><a href="#"><i className="fa fa-code"/> My Skills </a></li>
                       <li className="projectLink"><a href="#ProID"><i className="fa fa-briefcase"/> My Projects</a></li>
                       <li className="contactLink"><a href="#ContactID"><i className="fa fa-phone" /> Get in touch</a></li>
                 </ul>    
